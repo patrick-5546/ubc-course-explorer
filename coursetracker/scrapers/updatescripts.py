9d48d766b60a6dbfe9c4d160d50f8ae26a669ef2
Patrick Creighton <pat5546@student.ubc.ca>
2021-01-02 13:18:20 -0800

    added new line at end of file, renamed exp data file

--- coursetracker/scrapers/updatescripts.py
+++ coursetracker/scrapers/updatescripts.py
@@ -1,9 +1,9 @@
 import ubcexplorer as exp, ubcgrades as gr, ratemyprof as rmp, json
 
 # #To update rmp data (takes a couple minutes):
-ubcProfs = rmp.RateMyProfScraper(1413)  # 1413 is the school ID for UBC on Rate My Prof
-ubcProfs.update_rmp_data()
+# ubcProfs = rmp.RateMyProfScraper(1413)  # 1413 is the school ID for UBC on Rate My Prof
+# ubcProfs.update_rmp_data()
 
 # #To update preq data:
-# with open('coursetracker/scrapers/local_data/exp_all_course_preqs.txt', 'w') as outfile:
+# with open('coursetracker/scrapers/local_data/exp_courses_list.txt', 'w') as outfile:
 #     json.dump(exp.courses_info(), outfile)
