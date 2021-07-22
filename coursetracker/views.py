3478ab58a978614766e24b5d23662fb5248a8b81
patrick-5546 <pcreighton429@gmail.com>
2021-07-22 14:24:25 -0700

    Update views and html to match

--- coursetracker/views.py
+++ coursetracker/views.py
@@ -12,28 +12,21 @@ def search(request):
 
 
 def course(request, pk):
-    subAndCourse = pk.split(' ')
-    if len(subAndCourse) == 2:
-        subject = subAndCourse[0].upper()
-        course = subAndCourse[1]
-    else:
-        subject = pk[0:-3].upper()
-        # print(subject)
-        course = pk[-3:]
-        # print(course)
+    # initializing course_name so that the subject is all caps
+    course_name = pk
+    course_name_list = pk.split(' ')
+    if len(course_name_list) == 2:
+        course_name = f"{course_name_list[0].upper()} {course_name_list[1]}"
 
     try:
-        c = Course.objects.get(course_name__exact=subject + ' ' + course)
-        # print("getting course")
+        c = Course.objects.get(course_name__exact=course_name)
     except Course.DoesNotExist:
-        c = create_course(subject, course)
-        # print("creating new course")
-        if not c:
-            return render(request, 'coursetracker/404.html')
+        return render(request, 'coursetracker/404.html')
 
+    subject, course = course_name.split(' ')
     exp = ex.course_info_with_prereq_tree(subject, course)
     preq = {} if 'preq' not in exp else exp['preq']
-    preq = {subject + ' ' + course: preq}  # dictionary for tree chart
+    preq = {course_name: preq}  # dictionary for tree chart
 
     profsList = gr.teaching_team(subject, course)
 
@@ -52,40 +45,3 @@ def course(request, pk):
 
     return render(request, 'coursetracker/course.html', {'course': c, 'preq': preq, 'professors_info': profs,
                                                          'sections_taught': sectionProfsSorted})
-
-
-def create_course(subject, course):
-    if not gr.course_is_valid(subject, course):
-        return None
-
-    # TODO: decide whether to get term grade statistics (for high, low, pass, fail, etc.)
-    stats = gr.course_statistics(subject, course)
-    avg5 = stats['average_past_5_yrs']
-    stdev = stats['stdev']
-
-    # Jan 3, 2021
-    # Added minimum and maximum grade averages
-    minavg = stats['min_course_avg']
-    maxavg = stats['max_course_avg']
-
-    # TODO: consider differentiating between no data and 0
-    disInfo = gr.latest_distribution_info(subject, course)
-    distribution = [grade if grade else 0 for grade in list(disInfo['grades'].values())]
-    disTerm = disInfo['year'] + disInfo['session']
-
-    exp = ex.course_info_with_prereq_tree(subject, course)
-    creq = "n/a" if 'creq' not in exp else exp['creq']
-    depn = "n/a" if 'depn' not in exp else exp['depn']
-    name = "n/a" if 'name' not in exp else exp['name']
-    cred = "n/a" if 'cred' not in exp else exp['cred']
-    desc = "n/a" if 'desc' not in exp else exp['desc']
-    prer = "n/a" if 'prer' not in exp else exp['prer']
-    crer = "n/a" if 'crer' not in exp else exp['crer']
-    link = "n/a" if 'link' not in exp else exp['link']
-
-    c = Course(course_name=subject + ' ' + course, five_year_average=avg5, lowest_average=minavg,
-               highest_average=maxavg, standard_deviation=stdev, distribution=distribution, distribution_term=disTerm,
-               corequisites=creq, dependencies=depn, sub_name=name, number_of_credits=cred, course_description=desc,
-               prerequistes_description=prer, corequisites_description=crer, course_link=link)
-    c.save()
-    return c
