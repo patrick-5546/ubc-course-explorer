c05abc86cf7fcd7d1f540ecebff8720b9c35d261
patrick-5546 <pcreighton429@gmail.com>
2021-07-22 14:14:49 -0700

    Add average field, decrease max_length params

--- coursetracker/models.py
+++ coursetracker/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 
 class Course(models.Model):
-    course_name = models.CharField(max_length=200, default='')
-    five_year_average = models.CharField(max_length=200, default='0')
-    lowest_average = models.CharField(max_length=200, default='0')
-    highest_average = models.CharField(max_length=200, default='0')
-    standard_deviation = models.CharField(max_length=200, default='0')
-    distribution = models.TextField(default='')  # for the graph
-    distribution_term = models.CharField(max_length=200, default='0')
-    corequisites = models.TextField(default='')
-    dependencies = models.TextField(default='')
-    sub_name = models.CharField(max_length=200, default='')
-    number_of_credits = models.CharField(max_length=200, default='0')
-    course_description = models.TextField(default='')
-    prerequistes_description = models.TextField(default='')
-    corequisites_description = models.TextField(default='')
-    course_link = models.TextField(default='')
+    course_name = models.CharField(max_length=10, default='err')
+    average = models.CharField(max_length=20, default='err')
+    five_year_average = models.CharField(max_length=20, default='err')
+    lowest_average = models.CharField(max_length=20, default='err')
+    highest_average = models.CharField(max_length=20, default='err')
+    standard_deviation = models.CharField(max_length=20, default='err')
+    distribution = models.TextField(default='err')
+    distribution_term = models.CharField(max_length=5, default='err')
+    corequisites = models.TextField(default='err')
+    dependencies = models.TextField(default='err')
+    sub_name = models.CharField(max_length=100, default='err')
+    number_of_credits = models.CharField(max_length=5, default='err')
+    course_description = models.TextField(default='err')
+    prerequistes_description = models.TextField(default='err')
+    corequisites_description = models.TextField(default='err')
+    course_link = models.TextField(default='err')
 
     def __str__(self):
         return self.course_name
