f46e509dcaa8b834f3b23b602f2d897c739f3479
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-07-27 22:20:00 -0700

    docker-compose updates

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,7 @@
-# pull official base image
-FROM python:3.8.7-slim-buster
-
-# set work directory
-WORKDIR /app
-
-# install dependencies
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-
-# copy project
-COPY . .
-
-# run server cmd
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+# syntax=docker/dockerfile:1
+FROM python:3
+ENV PYTHONUNBUFFERED=1
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+COPY . /code/
--- ubc_course_explorer/settings.py
+++ ubc_course_explorer/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'ubc_course_explorer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR, 'db.sqlite3'))
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, 'db.sqlite3'))
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
