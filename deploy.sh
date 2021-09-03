0cc3f9cc4c4600ecf25040c59a3c047b27e3f039
patrick-5546 <pcreighton429@gmail.com>
2021-09-03 09:47:29 -0700

    Use production docker compose for ssl certificates

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ ssh -o StrictHostKeyChecking=no ubuntu@$1 << 'ENDSSH'
   aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 115646757360.dkr.ecr.us-east-2.amazonaws.com
   docker pull 115646757360.dkr.ecr.us-east-2.amazonaws.com/django-ec2-web
   docker pull 115646757360.dkr.ecr.us-east-2.amazonaws.com/django-ec2-nginx-proxy
-  docker-compose -f docker-compose.staging.yml up -d
-  docker-compose -f docker-compose.staging.yml exec -T web python manage.py migrate --noinput ; docker-compose -f docker-compose.staging.yml exec -T web python manage.py collectstatic --noinput --clear
+  docker-compose -f docker-compose.prod.yml up -d
+  docker-compose -f docker-compose.prod.yml exec -T web python manage.py migrate --noinput ; docker-compose -f docker-compose.prod.yml exec -T web python manage.py collectstatic --noinput --clear
 ENDSSH
